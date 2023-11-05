48de9ce47165b98f875216e496af8b5e1abac93f
FrancoDavid <franco.avalos@live.com>
2023-11-04 21:48:12 -0300

    feat: card front layout

--- interactive-card-form/src/app/app.module.ts
+++ interactive-card-form/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardFrontComponent } from './components/card-front/card-front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardFrontComponent
   ],
   imports: [
     BrowserModule,
